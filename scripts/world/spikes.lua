e94fba40292f3ba9afe381dd8bd8fdc23a47163d
flotts <git@flotts.io>
2018-10-07 16:02:49 -0400

    renamed spikes sprite

--- scripts/world/spikes.lua
+++ scripts/world/spikes.lua
@@ -2,13 +2,13 @@ spikes = {}
 spikes.allSpikes = {}
 
 function spikes:load()
-    spikesSprite = love.graphics.newImage("/assets/spikes.png")
+    spikes.spr = love.graphics.newImage("/assets/spikes.png")
 
     spikes:new(4, 5, 0)
     spikes:new(5, 4, 1)
     spikes:new(5, 14, 2)
     spikes:new(-2, -3, 2)
-    -- love.graphics.draw(spikesSprite, 200, 200, 0, 1, 1)
+    -- love.graphics.draw(spikes.spr, 200, 200, 0, 1, 1)
 end
 
 function spikes:new(x, y, rot)
@@ -40,6 +40,6 @@ function spikes:draw()
         local xVal = spikes.allSpikes[i].x
         local yVal = spikes.allSpikes[i].y 
         local rot = spikes.allSpikes[i].rot
-        love.graphics.draw(spikesSprite, xVal, yVal, rot, 1, 1, 8, 8)
+        love.graphics.draw(spikes.spr, xVal, yVal, rot, 1, 1, 8, 8)
     end
 end
