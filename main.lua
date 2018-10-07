eb97af5161ed3e6091cc502a6eafbb96a8ece3b0
flotts <git@flotts.io>
2018-10-07 18:18:55 -0400

    added zoom functionality (scroll wheel)

--- main.lua
+++ main.lua
@@ -6,7 +6,8 @@ require "scripts/cake"
 function love.load()
     screen = {
         width = love.graphics.getWidth(),
-        height = love.graphics.getHeight()
+        height = love.graphics.getHeight(),
+        scale = 3
     }
     
     love.graphics.setDefaultFilter("nearest")
@@ -25,6 +26,14 @@ function love.keypressed(key)
     end
 end
 
+function love.wheelmoved(x, y)
+    if screen.scale > 1 and y < 0 then
+        screen.scale = screen.scale + y
+    elseif screen.scale < 10 and y > 0 then
+        screen.scale = screen.scale + y
+    end
+end
+
 function love.update(dt)
     screen.width = love.graphics.getWidth()
     screen.height = love.graphics.getHeight()
@@ -35,7 +44,7 @@ end
 
 function love.draw()
     love.graphics.translate(screen.width / 2, screen.height / 2)
-    love.graphics.scale(2)
+    love.graphics.scale(screen.scale)
     love.graphics.rotate(cake.rot.rot)
     love.graphics.translate(-cake.body:getX(), -cake.body:getY())
 
